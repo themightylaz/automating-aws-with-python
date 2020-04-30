f9c4bbb0be2e954d63c98780f969c8208e627740
Lars Nielsen <themightylaz@gmail.com>
2020-04-30 17:39:52 +0200

    Output website when syncing

--- 01-webotron/webotron/bucket.py
+++ 01-webotron/webotron/bucket.py
@@ -4,6 +4,7 @@
 from pathlib import Path
 import mimetypes
 from botocore.exceptions import ClientError
+import util
 
 
 class BucketManager:
@@ -13,6 +14,18 @@ class BucketManager:
         """Create a BucketManager object."""
         self.session = session
         self.s3 = self.session.resource('s3')
+
+    def get_region_name(self, bucket):
+        """Get the bucket's region name."""
+        bucket_location = self.s3.meta.client.get_bucket_location(Bucket=bucket.name)
+        return bucket_location["LocationConstraint"] or 'us-east-1'
+
+    def get_bucket_url(self, bucket):
+        """Get the website URL for this bucket."""
+        return "http://{}.{}".format(
+            bucket.name,
+            util.get_endpoint(self.get_region_name(bucket)).host
+        )
 
     def all_buckets(self):
         """Get an iterator for all buckets."""
--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -20,6 +20,7 @@ from bucket import BucketManager
 session = None
 bucket_manager = None
 
+
 @click.group()
 @click.option('--profile', default=None, help="Use a given AWS profile.")
 def cli(profile):
@@ -65,6 +66,7 @@ def setup_bucket(bucket):
 def sync(pathname, bucket):
     """Sync contents of PATHNAME to BUCKET."""
     bucket_manager.sync(pathname, bucket)
+    print(bucket_manager.get_bucket_url(bucket_manager.s3.Bucket(bucket)))
 
 
 if __name__ == '__main__':
