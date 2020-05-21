7b1fbc5ea744c32431a33cb37d2eca6af6f31d3a
Lars Nielsen <themightylaz@gmail.com>
2020-05-21 20:41:17 +0200

    Add configuration for notifier service.

--- 02-notifon/notifier/handler.py
+++ 02-notifon/notifier/handler.py
@@ -1,24 +1,9 @@
-import json
+import os
 
 
-def hello(event, context):
-    # body = {
-    #     "message": "Go Serverless v1.0! Your function executed successfully!",
-    #     "input": event
-    # }
-    #
-    # response = {
-    #     "statusCode": 200,
-    #     "body": json.dumps(body)
-    # }
-    #
-    # return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
+def post_to_slack(event, context):
+    slack_webhook_url = os.environ['SLACK_WEBHOOK_URL']
+    print(slack_webhook_url)
     print(event)
 
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
+    return 
